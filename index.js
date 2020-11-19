88b688e568e904d696c7ac058af2e7300526e2a2
Dimitar Micevski <dmicevski@hotmail.com>
2020-11-18 21:17:59 -0500

    created some questions to gather user input

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
@@ -16,6 +24,32 @@ function generateMarkdown(data) {
 
   // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please provide a brief description of your project:',
+        name: 'description'
+    },
+    {
+        type: 'choices',
+        message: 'Please select the sections you would like to include in your Table of Contents:',
+        choices: [
+            "Installaion",
+            "Usage",
+            "License",
+            "Contributing",
+            "Tests",
+            "Questions"
+        ],
+        name: 'tableofcontents'
+    },
+    {
+        
+    }
 
 ];
 
